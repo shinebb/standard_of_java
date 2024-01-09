a1e2a4080f883a4125a8aa724790eeb219de0479
Dohee Kim <ododalrio@gmail.com>
2024-01-09 22:49:41 +0900

    240109 변수

---
+++ chapter02/Ex_print.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+public class Ex_print {
+    public static void main(String[] args){
+        //print() : 출력 후에 줄바꿈 안함
+        //println() : 출력 후에 줄바꿈 함
+
+        System.out.print("Hello");
+        System.out.print("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
+        System.out.println(5+3);    //덧셈
+        System.out.println(5-3);    //뺄셈
+        System.out.println(5*3);    //곱셈
+        System.out.println(5/3);    //나눗셈
+    }
+}
---
+++ chapter02/Ex_variable.java
@@ -0,0 +1,17 @@
+package chapter02;
+
+public class Ex_variable {
+    public static void main(String[] args){
+
+        System.out.println("6+3");
+        System.out.println(6+3);
+
+        //변수 선언 초기화 및 읽어오기
+        int x = 4, y = 2;
+        System.out.println(x+y); //덧셈
+        System.out.println(x-y); //뺄셈
+        System.out.println(x*y); //곱셈
+        System.out.println(x/y); //나눗셈
+
+    }
+}
