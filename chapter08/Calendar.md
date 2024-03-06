db635cac8566f1ad4e9c04bbeba6cea81723c152
dohee <ododalrio@gmail.com>
2024-03-06 12:24:39 +0900

    240306 DecimalFormat, SimpleDateFormat

--- chapter08/Calendar.md
+++ chapter08/Calendar.md
@@ -170,8 +170,86 @@ Date와 Calendar간의 변환
 
 
 
+형식화 클래스
+======================
+
+* java.text 패키지의 DecimalFormat(숫자), SimpleDateFormat(날짜)
+* 숫자와 날짜를 원하는 형식으로 쉽게 출력 가능(숫자, 날짜 -> 형식 문자열)
+* 형식 문자열에서 숫자와 날짜를 뽑아내는 기능(형식 문자열 -> 숫자, 날짜)
 
 
+DecimalFormat
+------------------
+*****
+
+* 숫자(10진수)를 형식화할 때 사용(숫자->형식문자열)
+
+
+    double number = 1234567.89;
+    DecimalFormat df = new DecimalFormat("#.#E0"); //지수형식 표현
+    String result = df.format(number); //result = "1.2E6"
+
+* 특정 형식의 문자열을 숫자로 변환할 때도 사용(형식문자열->숫자)
+
+
+    DecimalFormat df = new DecimalFormat("#,###.##"); 
+    Number num = df.parse("1,234,567.89"); //Number : 모든 숫자 래퍼클래스의 최고 조상
+    double d = num.doubleValue(); //1234567.89
+
+
+* [참고] Integer.parseInt(), Float.parseFloat()는 콤마(,)가 포함된 문자열을 숫자로 변환 못함
+
+
+SimpleDateFormat
+----------
+*****
+
+* 날짜와 시간을 다양한 형식으로 출력할 수 있게 해준다.
+
+  
+    Date today = new Date();
+    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+    //오늘 날짜를 yyyy-MM-dd 형태로 변환하여 반환한다.
+    String result = df.format(today);
+
+    
+<br>
+
+* G : 연대(BC, AD)
+* y : 년도 
+* M : 월(1~12 또는 1월~12월)
+* w : 년의 몇 번째 주(1~53)
+* W : 월의 몇 번째 주(1~5)
+* D : 년의 몇 번째 일(1~366)
+* d : 월의 몇 번째 일(1~31)
+* F : 월의 몇 번째 요일(1~5)
+* E : 요일
+
+<br>
+
+* 특정 형식으로 되어 있는 문자열에서 날짜와 시간을 뽑아낼 수도 있다.
+
+
+    DateFormat df = new SimpleDateFormat("yyyy년 MM월 dd일");
+    DateFormat df2 = new SimpleDateFormat("yyyy/MM/dd);
+    Date d = df.parse("2025년 11월 23일); //문자열을 Date로 변환
+    String reuslt = df2.format(d);
+
+
+<br>
+
+* a : 오전/오후(AM,PM)
+* H : 시간(0~23)
+* k : 시간(1~24)
+* K : 시간(0~11)
+* h : 시간(1~12)
+* m : 분(0~59)
+* s : 초(0~59)
+* S : 천분의 일초(0~999)
+* z : Time zone(General time zone)
+* Z : Time zone(RFC 822 time zone)
+* ' : escape 문자(특수문자를 표현하는데 사용)
 
 
 
---
+++ chapter08/Ex_simpleDateFormat.java
@@ -0,0 +1,50 @@
+package chapter08;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Ex_simpleDateFormat {
+    public static void main(String[] args){
+
+        Date today = new Date();
+
+        SimpleDateFormat sdf1, sdf2, sdf3, sdf4, sdf5, sdf6, sdf7, sdf8, sdf9;
+
+        sdf1 = new SimpleDateFormat("yyyy-mm-dd");
+        sdf2 = new SimpleDateFormat("''yy년 MMM dd일 E요일");
+        sdf3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        sdf4 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a");
+
+        sdf5 = new SimpleDateFormat("오늘은 올해의 D번째 날입니다.");
+        sdf6 = new SimpleDateFormat("오늘은 이 달의 d번째 날입니다.");
+        sdf7 = new SimpleDateFormat("오늘은 올해의 w번째 주입니다.");
+        sdf8 = new SimpleDateFormat("오늘은 이 달의 W번째 주입니다.");
+        sdf9 = new SimpleDateFormat("오늘은 이 달의 F번째 E요일입니다.");
+
+        System.out.println(sdf1.format(today));
+        System.out.println(sdf2.format(today));
+        System.out.println(sdf3.format(today));
+        System.out.println(sdf4.format(today));
+        System.out.println();
+        System.out.println(sdf5.format(today));
+        System.out.println(sdf6.format(today));
+        System.out.println(sdf7.format(today));
+        System.out.println(sdf8.format(today));
+        System.out.println(sdf9.format(today));
+
+
+        DateFormat df = new SimpleDateFormat("yyyy년 MM월 dd일");
+        DateFormat df2 = new SimpleDateFormat("yyyy/MM/dd");
+
+        try{
+            Date d = df.parse("2019년 11월 23일"); //문자열을 날짜로
+            System.out.println(df2.format(d)); //날짜를 문자열로
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
